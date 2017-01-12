22fd6362d29665e43cdd663283f6d2d3b17e90c2
primasalama <inbox.prima@gmail.com>
2017-01-12 15:08:45 +0700

    front

---
+++ application/views/frontend/footer.php
@@ -0,0 +1,13 @@
+
+<footer class="footer navbar-fixed-bottom" style="height: 130px">
+      <div class="container">
+        <p class="pull-left" style="margin-left: -50px">Deputi Bidang Koordinasi Sumber Daya Alam dan Jasa<br />
+Kementerian Koordinator Bidang Kemaritiman<br />
+Jl. M. H. Thamrin No. 8, Gedung I BPPT Lt.6 ,Jakarta Pusat 10340<br />
+Telpon (021) 22392589, Fax (021) 22392589<br />
+Email : deputi2_sdaj@maritim.go.id <br />
+Jawa Barat, Indonesia</p>
+      </div>
+    </footer>
+</body>
+</html>
---
+++ application/views/frontend/header.php
@@ -0,0 +1,66 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="<?php echo base_url();?>assets/img/smanbul.png">
+    <title>Front | Simoniks</title>
+
+    
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url();?>assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>assets/css/jquery.dataTables.min.css" rel="stylesheet">
+    <!---<link href="<?php echo base_url();?>assets/css/jquery.timepicker.min.css" rel="stylesheet">-->
+    <link href="<?php echo base_url();?>assets/css/jquery-ui.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="<?php echo base_url();?>assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url();?>assets/css/dashboard.css" rel="stylesheet">
+
+    <script src="<?php echo base_url();?>assets/js/ie-emulation-modes-warning.js"></script>
+
+       <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="<?php echo base_url();?>assets/js/jquery.min.js"></script>
+    <script src="<?php echo base_url();?>assets/js/jquery-ui.js"></script>
+    <script src="<?php echo base_url();?>assets/js/jquery.dataTables.min.js"></script>
+    <script>window.jQuery || document.write('<script src="<?php echo base_url();?>assets/js/jquery.min.js"><\/script>')</script>
+    <script src="<?php echo base_url();?>assets/js/bootstrap.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="<?php echo base_url();?>assets/js/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="<?php echo base_url();?>assets/js/ie10-viewport-bug-workaround.js"></script>
+
+  </head>
+
+  <body>
+  <nav class="navbar navbar-default navbar-fixed-top" style="height: 130px">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      
+      <div class="navbar-brand" style="text-align:center; margin-left: 350px;">DEPUTI BIDANG KOORDINASI SUMBER DAYA DAN JASA <br /><br/>
+      KEMENTRIAN KOORDINATOR BIDANG KEMARITIMAN</div>
+      
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    
+  </div><!-- /.container-fluid -->
+</nav>
+<style type="text/css">
+  #read{
+    pointer-events: none;
+       cursor: default;
+  }
+</style>
