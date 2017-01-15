e7598669a475459452c7cde8c08689c84c756d64
arif <arif@aksimaya.co.id>
2017-01-15 09:10:05 +0700

    PHP EXCEL

--- application/controllers/Beranda.php
+++ application/controllers/Beranda.php
@@ -24,6 +24,7 @@ class Beranda extends CI_Controller {
 		$this->load->model('M_agenda');
 		$this->load->model('M_kebijakan');
 		$this->load->model('M_progress');
+		$this->load->library('excel');
 	}
 	public function index()
 	{
@@ -66,7 +67,29 @@ class Beranda extends CI_Controller {
 	}
 	public function coba($value='')
 	{
-		echo "fungsi coba";
-		echo "<br/>".$this->uri->Segment(3);
+		//activate worksheet number 1
+$this->excel->setActiveSheetIndex(0);
+//name the worksheet
+$this->excel->getActiveSheet()->setTitle('test worksheet');
+//set cell A1 content with some text
+$this->excel->getActiveSheet()->setCellValue('A1', 'This is just some text value');
+//change the font size
+$this->excel->getActiveSheet()->getStyle('A1')->getFont()->setSize(20);
+//make the font become bold
+$this->excel->getActiveSheet()->getStyle('A1')->getFont()->setBold(true);
+//merge cell A1 until D1
+$this->excel->getActiveSheet()->mergeCells('A1:D1');
+//set aligment to center for that merged cell (A1 to D1)
+$this->excel->getActiveSheet()->getStyle('A1')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+$filename='just_some_random_name.xls'; //save our workbook as this file name
+header('Content-Type: application/vnd.ms-excel'); //mime type
+header('Content-Disposition: attachment;filename="'.$filename.'"'); //tell browser what's the file name
+header('Cache-Control: max-age=0'); //no cache
+            
+//save it to Excel5 format (excel 2003 .XLS file), change this to 'Excel2007' (and adjust the filename extension, also the header mime type)
+//if you want to save it as .XLSX Excel 2007 format
+$objWriter = PHPExcel_IOFactory::createWriter($this->excel, 'Excel5');  
+//force user to download the Excel file without writing it to server's HD
+$objWriter->save('php://output');
 	}
 }
