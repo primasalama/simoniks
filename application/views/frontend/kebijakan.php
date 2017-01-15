ae1b0fe20587c824b063e9d5c2b69695d7a885e9
primasalama <inbox.prima@gmail.com>
2017-01-15 15:21:24 +0700

    update

--- application/views/frontend/kebijakan.php
+++ application/views/frontend/kebijakan.php
@@ -91,7 +91,8 @@
     <nav class="navbar navbar-default navbar-fixed-bottom" style="height: 130px; background-color: #6699FF">
     <div class="navbar-footer">
         <p class="pull-left" style="margin-left: 40px; margin-top: 10px; font-size: 10pt; color: white">Deputi Bidang Koordinasi Sumber Daya Alam dan Jasa<br />
-        Kementerian Koordinator Bidang Kemaritiman<br />
+        Kementerian Koordinator Bidang Kemaritiman</p>
+        <p class="pull-right" style="margin-right: : 40px; margin-top: 10px; font-size: 10pt; color: white">
         Jl. M. H. Thamrin No. 8, Gedung I BPPT Lt.6 ,Jakarta Pusat 10340<br />
         Telpon (021) 22392589, Fax (021) 22392589<br />
         Email : deputi2_sdaj@maritim.go.id <br />
