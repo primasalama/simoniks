cb759a4385bf8804a99b5acb71deb3d0c8e906a4
bara ismiaji <baraismiaji@gmail.com>
2017-02-02 15:04:54 +0700

    up

--- application/controllers/fagenda.php
+++ application/controllers/fagenda.php
@@ -38,7 +38,7 @@ class Fagenda extends CI_Controller {
 	public function add()
 	{
 		//$tgl = substr($this->input->post('tanggal'), 6,4)."-".substr($this->input->post('tanggal'), 0,2)."-".substr($this->input->post('tanggal'), 3,2);
-		$data = array('narasiKebijakan'=>$this->input->post('narasiKebijakan'),'uraian'=>$this->input->post('uraian'),'tanggal'=>$this->input->post('tanggal'),'hasil'=>$this->input->post('hasil'),'created_by'=>$this->session->userdata('session')[0]->no,'updated_by'=>$this->session->userdata('session')[0]->no);
+		$data = array('narasiKebijakan'=>$this->input->post('narasiKebijakan'),'uraian'=>nl2br($this->input->post('uraian')),'tanggal'=>$this->input->post('tanggal'),'hasil'=>nl2br($this->input->post('hasil')),'created_by'=>$this->session->userdata('session')[0]->no,'updated_by'=>$this->session->userdata('session')[0]->no);
 		$this->M_agenda->insert($data);
 		redirect('agenda/'.$this->session->userdata('session')[0]->role);
 	}
@@ -53,7 +53,7 @@ class Fagenda extends CI_Controller {
 	}
 	public function update($value)
 	{
-		$data = array('narasiKebijakan' => $this->input->post('narasiKebijakan'),'uraian'=>$this->input->post('uraian'),'tanggal'=>$this->input->post('tanggal'),'hasil'=>$this->input->post('hasil'),'updated_by'=>$this->session->userdata('session')[0]->no,'updated_at'=>date("Y-m-d H:i:s"));
+		$data = array('narasiKebijakan' => $this->input->post('narasiKebijakan'),'uraian'=>nl2br($this->input->post('uraian')),'tanggal'=>$this->input->post('tanggal'),'hasil'=>nl2br($this->input->post('hasil')),'updated_by'=>$this->session->userdata('session')[0]->no,'updated_at'=>date("Y-m-d H:i:s"));
 		$this->M_agenda->updateId($data,$value);
 		redirect('agenda/'.$this->session->userdata('session')[0]->role);
 	}
