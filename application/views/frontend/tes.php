45e63cb057c655037bd0cdc14cc672a2562f4df9
123456-PC\123456 <inbox.prima@gmail.com>
2017-04-02 23:09:19 +0700

    tes

--- application/views/frontend/tes.php
+++ application/views/frontend/tes.php
@@ -2,12 +2,31 @@
 <html>
 <head>
 <link href="<?php echo base_url();?>assets/css/bootstrap.css" rel="stylesheet" media="screen">
+
+<link href="<?php echo base_url();?>assets/css/jquery.dataTables.css" rel="stylesheet" media="screen">
 	<title></title>
 	<script src="<?php echo base_url();?>assets/js/jquery.js"></script>
         <script src="<?php echo base_url();?>assets/js/bootstrap.js"></script>
         <script src="<?php echo base_url();?>assets/js/jquery.dataTables.js"></script>
 </head>
 <body>
+	<style type="text/css">
+body {
+  font: 90%/1.45em "Helvetica Neue", HelveticaNeue, Verdana, Arial, Helvetica, sans-serif;
+  margin: 0;
+  padding: 0;
+  color: #333;
+  background-color: #fff;
+}
+
+
+div.container {
+  min-width: 980px;
+  margin: 0 auto;
+}
+
+
+	</style>
 <script type="text/javascript">
             $(document).ready(function() {
               $('#kebijakan').DataTable();
@@ -17,9 +36,9 @@
 	//print_r($kprogress->num_rows());die();
 	$z=1;
 	$data = $kprogress->result();
-	for ($i=0; $i <= $kprogress->num_rows() ; $i++) { 
+	for ($i=0; $i < $kprogress->num_rows() ; $i++) { 
 		# code...
-		if ($i != $kprogress->num_rows()) {
+		if ($i > $kprogress->num_rows()) {
 			if ($data[$i]->narasiKebijakan != $data[$i+1]->narasiKebijakan) {
 				# code...
 				$baris[$i][0] = $z ;$z++;
@@ -40,20 +59,38 @@
   
 	var data = <?php echo json_encode($baris);?>;
 	console.log(data);
+	 var table = $('#example').DataTable({
+    columns: [
+        {
+            name: 'first',
+            title: 'No',
+        },
+        {
+            name: 'second',
+            title: 'Narasi',
+        },
+        {
+            title: 'Uraian',
+        }, 
+        {
+            title: 'Tindak Lanjut',
+        },
+        {
+            title: 'Masalah',
+        },
+
+    ],
+    data: data,
+    rowsGroup: [
+      'first:name',
+      'second:name',
+    ],
+    pageLength: '10',
+    });
 });
 </script>
-	<table id="" border="1">
-		<thead>
-			<tr>
-				<th width="2%">No.</th>
-				<th width="30%">Narasi</th>
-				<th width="20%">Kegiatan</th>
-				<th width="20%">Hasil</th>
-			</tr>
-		</thead>
-		<tbody>
+	<table id="example" border="1">
 		
-		</tbody>
 	</table>
 </body>
 </html>
