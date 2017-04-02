20e9fd0d969a5ef28b84544412d66f7333d29484
123456-PC\123456 <inbox.prima@gmail.com>
2017-04-02 22:56:47 +0700

    commit

--- application/views/frontend/tes.php
+++ application/views/frontend/tes.php
@@ -15,43 +15,32 @@
 </script>
 <?php 
 	//print_r($kprogress->num_rows());die();
-	$z=0;
+	$z=1;
 	$data = $kprogress->result();
-	for ($i=0; $i < $kprogress->num_rows() ; $i++) { 
+	for ($i=0; $i <= $kprogress->num_rows() ; $i++) { 
 		# code...
-		$baris[$i][1] = () ? a : b ;
+		if ($i != $kprogress->num_rows()) {
+			if ($data[$i]->narasiKebijakan != $data[$i+1]->narasiKebijakan) {
+				# code...
+				$baris[$i][0] = $z ;$z++;
+			}else{
+				$baris[$i][0] = $z ;
+			}
+		}else{
+			$baris[$i][0] = $z ;
+		}
+		$baris[$i][1] = $data[$i]->narasi;
+		$baris[$i][2] = $data[$i]->uraian;
+		$baris[$i][3] = $data[$i]->tindak_ljt;
+		$baris[$i][4] = $data[$i]->masalah;
 	}
 	?>
 <script type="text/javascript">
 	$(document).ready( function () {
   
-	var data = <?php echo json_encode($kprogress);?>;
-  var table = $('#example').DataTable({
-    columns: [
-        {
-            name: 'first',
-            title: 'ID',
-        },
-        {
-            name: 'second',
-            title: 'Name',
-        },
-        {
-            title: 'Subject',
-        }, 
-        {
-            title: 'Marks',
-        },
-    ],
-    data: data,
-    rowsGroup: [
-      'first:name',
-      'second:name'
-    ],
-    pageLength: '20',
-    });
-} );
-  console.log(data);
+	var data = <?php echo json_encode($baris);?>;
+	console.log(data);
+});
 </script>
 	<table id="" border="1">
 		<thead>
